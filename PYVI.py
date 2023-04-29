c65496a5ddbcffb895ced044683e2d6a9e4be7f0
YORLEIS <106856891+YORLEIS@users.noreply.github.com>
2023-04-29 18:46:34 -0500

    Update PYVI.py

--- PYVI.py
+++ PYVI.py
@@ -4,3 +4,6 @@ Calcular el numero de pulsaciones que una persona debe tener por cada 10 segundo
 si la formula es: num. pulsaciones = (220 - edad)/10
 """
 # Tu código debajo de esta línea
+edad=int(input("ingrese la edad: "))
+num_pulsaciones = (220 - edad)/10
+print("su numero de pulsaciones es", num_pulsaciones)
