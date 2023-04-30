d17be99ad81b4afee6c71328cfde684b78061fbd
Yorleis Arias <ariasrochayorleis@gmail.com>
2023-04-29 20:55:36 -0500

    subiendo_ejercicio_7

--- PYVII.py
+++ PYVII.py
@@ -7,3 +7,4 @@ C=5/9*(f-32)
 f=int(input("ingrese los grados farenheit: "))
 C=5/9*(f-32)
 print(f"la conversion de los {f} grados farenheit a centigrados es: {C}°C")
+#subiendo el ejercicio al report
